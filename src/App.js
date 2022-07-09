eff5e68a95f3c3618b566e8084e2e9db035298fb
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2022-07-09 16:37:56 +0200

    useCategoriesData

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
-import { GlobalStyle } from './GlobalStyles'
+import { GlobalStyle } from './styles/GlobalStyles'
 import { PhotoCard } from './components/PhotoCard'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { Logo } from './components/Logo'
 
 export const App = () => (
   <div>
     <GlobalStyle />
+    <Logo />
     <ListOfCategories />
     <PhotoCard />
     <ListOfPhotoCards />
--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Category } from '../Category'
 import db from '../../../api/db.json'
-
 import { List, Item } from './styles'
+/* import { categories as mockCategories } from '../../../api/db.json' */
 
 export const ListOfCategories = () => {
+
+  const [categories, setCategories] = useState();
+
+  useEffect(() => {
+    fetch('https://mocki.io/v1/14701bc5-e6ab-4393-a7b0-77724af93767')
+      .then (res => res.json())
+      .then (response => {
+        setCategories(response)
+      })
+
+  }, []);
+
   return (
     <List>
       {db.categories.map((category) => (

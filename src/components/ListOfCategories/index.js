efbc5b04a67c1ade9076bb02219d188b4113cc4d
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2022-07-11 18:59:30 +0200

    17/50

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,30 +1,43 @@
 import React, { useState, useEffect } from 'react'
 import { Category } from '../Category'
-import db from '../../../api/db.json'
 import { List, Item } from './styles'
-/* import { categories as mockCategories } from '../../../api/db.json' */
 
 export const ListOfCategories = () => {
-
-  const [categories, setCategories] = useState();
+  const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
 
   useEffect(() => {
-    fetch('https://mocki.io/v1/14701bc5-e6ab-4393-a7b0-77724af93767')
-      .then (res => res.json())
-      .then (response => {
-        setCategories(response)
-      })
+    setTimeout(() => {
+      fetch('https://mocki.io/v1/14701bc5-e6ab-4393-a7b0-77724af93767')
+        .then((response) => response.json())
+        .then((json) => {
+          setCategories(json.categories)
+        })
+    }, [])
+  }, 2000)
 
-  }, []);
+  useEffect(() => {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
+      {
+        categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
+      }
+    </List>
+  )
 
   return (
-    <List>
-      {db.categories.map((category) => (
-        // eslint-disable-next-line react/jsx-key
-        <Item key={category.id}>
-          <Category {...category} />
-        </Item>
-      ))}
-    </List>
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
